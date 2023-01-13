673b6696433db30f04f57ea775d19396ef1ab668
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-13 15:52:13 +0530

    fix: bug fixes in armstrong function and optimisation of prime checker

--- primeAbdArmstrong.js
+++ primeAbdArmstrong.js
@@ -1,5 +1,7 @@
 const isPrime= (n)=> {
   let isPrime= true;
+  if (n<= 2) return true;
+  if (n%2== 0) return false; 
   for (let i= 2; i< n/2; ++i){
     if (n%i== 0) {
       ifPrime= !isPrime;
@@ -9,13 +11,16 @@ const isPrime= (n)=> {
   } 
 }
 
+console.log(isPrime(13));
+
 const isArmStrong= (n)=> {
   let s= `${n}`;
-  let arr= []
-  for (let i= 0; i< s.length; ++i) {arr.push(Number(s[i]));}
+  // let arr= []
+  // for (let i= 0; i< s.length; ++i) {arr.push(Number(s[i]));}
   let cubeAdd= 0;
-  for (let i in arr) cubeAdd+= Math.pow(i, 3);
+  for (let i of s) cubeAdd+= Math.pow(Number(i), 3);
   return n=== cubeAdd;
 }
 
 
+console.log(isArmStrong(153));
